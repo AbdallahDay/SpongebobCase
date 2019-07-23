38cf855659c2c3deb2e8aa6e1058f0598eb00f23
Abdallah Day <10871403+AbdallahDay@users.noreply.github.com>
2019-07-22 23:53:18 -0500

    aAaAh

--- spongebobCase.js
+++ spongebobCase.js
@@ -5,7 +5,7 @@ convertToSpongebobCase = (str) => {
         let ch = str[i];
 
         if (ch.match('/^[a-zA-Z0-9]$/')) {
-            l++;    // "Letter" counter increments by one if current charcter is alphanumeric
+            l++;    // "Letter counter" increments by one if current charcter is alphanumeric
             
             if (l%2 != 0) {
                 // odd "letter" to lowercase
